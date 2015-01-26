669997600bba44367b61f1a7b0a4df5fa059f897
gratimax <max@ovsankin.com>
2015-01-26 11:37:09 -0800

    Add roboto font

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # spongedocs-theme
 The new, and improved, sponge documentation theme.
 More coming soon.
+
+# Development
+```bash
+npm install
+bower install
+gulp 
+```
--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,16 @@
 var gulp = require('gulp');
 
 // Gulp plugins
-var del = require('del');
+var filter = require('gulp-filter');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
 var sourcemaps = require('gulp-sourcemaps');
+var runSeq = require('run-sequence');
+
+// Build dependencies
+var del = require('del');
+var mainBowerFiles = require('main-bower-files');
 
 // Stylus includes
 var nib = require('nib');
@@ -14,13 +19,15 @@ var jeet = require('jeet');
 var typographic = require('typographic');
 
 // Stylus build configuration
-var styl = stylus({
-  use: [
-    nib(),
-    jeet(),
-    typographic()
-  ]
-});
+var styl = function () {
+  return stylus({
+    use: [
+      nib(),
+      jeet(),
+      typographic()
+    ]
+  });
+};
 
 // Cleans the build directory
 gulp.task('clean', function (cb) {
@@ -37,33 +44,48 @@ gulp.task('conf', function () {
 
 // Copies html templates
 gulp.task('html', function () {
-  return gulp.src('./src/html/')
-    .pipe(gulp.dest('./build'));
+  return gulp.src('./src/html/**/*')
+    .pipe(gulp.dest('./build/'));
 });
 
 // Compiles and minifies stylus
 gulp.task('styl', function () {
   return gulp.src('./src/styl/index.styl')
-    .pipe(styl)
+    .pipe(styl())
     .pipe(rename('sponge.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('./build/static/'));
 });
 
+// Copies fonts from bower
+gulp.task('fonts', function () {
+  return gulp.src(mainBowerFiles())
+    .pipe(filter([
+      '*.eot', '*.svg', '*.ttf', '*.woff', '*.woff2'
+    ]))
+    .pipe(gulp.dest('./build/static/fonts/'));
+});
+
 // Performs build
-gulp.task('build', ['clean', 'conf', 'styl']);
+gulp.task('build', function (cb) {
+  runSeq(
+    'clean',
+    ['fonts', 'conf', 'html', 'styl'],
+    cb
+  );
+});
 
 // Compiles stylus with sourcemaps
 gulp.task('styl-dev', function () {
   return gulp.src('./src/styl/index.styl')
     .pipe(sourcemaps.init())
-    .pipe(styl)
+    .pipe(styl())
     .pipe(sourcemaps.write())
     .pipe(rename('sponge.css'))
     .pipe(gulp.dest('./build/static/'));
 });
 
-gulp.task('build-dev', ['conf', 'styl-dev']);
+gulp.task('build-dev', ['fonts', 'conf', 'html', 'styl-dev']);
 
 // Rebuild when files are changed
 gulp.task('dev', ['build-dev'], function (cb) {
