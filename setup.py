0b39a412f5a9c12abd7a70c85ec7662515b7857b
gratimax <max@ovsankin.com>
2015-03-16 20:33:08 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sponge_docs_theme',
+    version='0.0.1',
+    packages=['sponge_docs_theme'],
+    url='',
+    license='',
+    author='max',
+    author_email='',
+    description='The sponge documentation theme'
+)
