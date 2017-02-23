251e6f60cad21f5c20cc64d6ef5a0aa47f2c0f1d
Minecrell <dev@minecrell.net>
2017-02-23 14:01:23 +0100

    Move Sphinx extension to extra theme.py file

---
+++ src/theme/theme.py
@@ -0,0 +1,75 @@
+import os.path
+
+from sphinx.builders.html import StandaloneHTMLBuilder
+
+from . import __version__
+from .context import setup_html_context
+
+
+def strip_country(l):
+    return l[:l.index('_')]
+
+
+class FixedHTMLBuilder(StandaloneHTMLBuilder):
+    def _get_translations_js(self):
+        # Check the regular path
+        jsfile = super()._get_translations_js()
+        if jsfile or '_' not in self.config.language:
+            return jsfile
+
+        # Check again but with the country part stripped
+        old_language = self.config.language
+        self.config.language = strip_country(old_language)
+        jsfile = super()._get_translations_js()
+
+        # Restore old language
+        self.config.language = old_language
+        return jsfile
+
+
+def init(app):
+    # Return if we are not doing a HTML build
+    if app.builder.name != 'html':
+        return
+
+    # Workaround for https://github.com/sphinx-doc/sphinx/issues/2345
+    if app.config.language and '_' in app.config.language:
+        app.config.html_search_language = strip_country(app.config.language)
+
+    # Trim whitespace in resulting HTML
+    app.builder.templates.environment.trim_blocks = True
+    app.builder.templates.environment.lstrip_blocks = True
+
+
+def setup(app):
+    # Workaround for https://github.com/sphinx-doc/sphinx/issues/2345
+    del app.builderclasses['html']
+    app.add_builder(FixedHTMLBuilder)
+
+    local_dir = os.path.abspath(os.path.dirname(__file__))
+
+    # Set templates path
+    app.config.templates_path = [os.path.join(local_dir, 'templates')]
+
+    # Set HTML theme to Sphinx ReadTheDocs theme
+    app.config.html_theme = 'sphinx_rtd_theme'
+
+    # Setup HTML context
+    app.config.html_context = setup_html_context()
+
+    # Set HTML static path and favicon
+    app.config.html_static_path = [os.path.join(local_dir, 'static')]
+    app.config.html_favicon = os.path.join(local_dir, 'favicon.ico')
+
+    # Load helper extensions for our theme
+    app.setup_extension('sponge_docs_theme.languages')
+    app.setup_extension('sponge_docs_theme.helpers')
+
+    # Register listener to set options when builder was initialized
+    app.connect('builder-inited', init)
+
+    return {
+        'version': __version__,
+        'parallel_read_safe': True,
+        'parallel_write_safe': True
+    }
