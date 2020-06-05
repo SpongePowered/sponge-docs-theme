255bff8564559018a304188d0cdd2141d29d14c7
Felix Kleinekathöfer <felixoiDev@gmail.com>
2020-06-05 14:04:31 +0200

    Upgraded to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const
     gulp = require('gulp'),
-    gutil = require('gulp-util'),
+    PluginError = require('plugin-error'),
     concat = require('gulp-concat'),
     del = require('del'),
 
@@ -22,37 +22,40 @@ const
 
     spawn = require('child_process').spawn,
 
-    webserver = require('gulp-webserver');
+    connect = require('gulp-connect');
 
-gulp.task('clean', () => del(['sponge_docs_theme', 'dist', 'build']));
+function clean() {
+    return del(['sponge_docs_theme', 'dist', 'build']);
+}
 
 // Theme
-gulp.task('theme:files', () =>
-    gulp.src('src/theme/{*.*,{static,templates}/**}')
-        .pipe(gulp.dest('sponge_docs_theme'))
-);
 
-gulp.task('theme:svg', () =>
-    gulp.src('src/theme/svg/**')
+function themeFiles() {
+    return gulp.src('src/theme/{*.*,{static,templates}/**}')
+        .pipe(gulp.dest('sponge_docs_theme'));
+}
+
+function themeSVG() {
+    return gulp.src('src/theme/svg/**')
         .pipe(svgmin())
-        .pipe(gulp.dest('sponge_docs_theme/static'))
-);
+        .pipe(gulp.dest('sponge_docs_theme/static'));
+}
 
-gulp.task('theme:scripts', () =>
-    gulp.src('src/theme/scripts/**')
-        .pipe(gulp.dest('dist/scripts'))
-);
+function themeScripts() {
+    return gulp.src('src/theme/scripts/**')
+        .pipe(gulp.dest('dist/scripts'));
+}
 
-gulp.task('theme:scss', () =>
-    gulp.src('src/theme/scss/spongedocs.scss')
+function themeScss() {
+    return gulp.src('src/theme/scss/spongedocs.scss')
         .pipe(sass())
         .pipe(postcss([autoprefixer()]))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('sponge_docs_theme/static'))
-);
+        .pipe(gulp.dest('sponge_docs_theme/static'));
+}
 
-gulp.task('theme:js', () =>
-    gulp.src('src/theme/js/*.js')
+function themeJS() {
+    return gulp.src('src/theme/js/*.js')
         .pipe(concat('spongedocs.js'))
         .pipe(buble({
             transforms: {
@@ -64,59 +67,65 @@ gulp.task('theme:js', () =>
                 toplevel: true,
             },
         }))
-        .pipe(gulp.dest('sponge_docs_theme/static'))
-);
+        .pipe(gulp.dest('sponge_docs_theme/static'));
+}
 
-gulp.task('theme:js:worker', () =>
-    gulp.src('src/theme/js/offline/worker.js')
+function themeJSWorker() {
+    return gulp.src('src/theme/js/offline/worker.js')
         .pipe(buble())
         .pipe(uglify({
             mangle: {
                 toplevel: true
             }
         }))
-        .pipe(gulp.dest('sponge_docs_theme/extra'))
-);
+        .pipe(gulp.dest('sponge_docs_theme/extra'));
+}
 
-gulp.task('theme:js:lib', () =>
-    gulp.src('src/theme/js/lib/*.js')
+function themeJSLib() {
+    return gulp.src('src/theme/js/lib/*.js')
         .pipe(uglify({
             output: {
                 comments: 'some'
             }
         }))
-        .pipe(gulp.dest('sponge_docs_theme/static'))
-);
+        .pipe(gulp.dest('sponge_docs_theme/static'));
+}
 
+function themeJSgetText() {
+    return shell('babel', 'python', ['setup.py', 'extract_messages', '-o', 'sponge_docs_theme/theme.pot']);
+}
 
-gulp.task('theme:js:gettext', ['theme:files'],
-    shell('babel', 'python', ['setup.py', 'extract_messages', '-o', 'sponge_docs_theme/theme.pot'])
-);
+function watch() {
+    gulp.watch('src/theme/{*.py,{static,templates}/**}', themeFiles);
+    gulp.watch('src/theme/svg/**', themeSVG);
+    gulp.watch('src/theme/scripts/**', themeScripts);
+    gulp.watch('src/theme/scss/**', themeScss);
+    gulp.watch('src/theme/js/*.js', gulp.series(themeJS, themeJSgetText));
+    gulp.watch('src/theme/js/offline/worker.js', themeJSWorker);
+}
 
-gulp.task('theme:build', ['theme:files', 'theme:svg', 'theme:scripts', 'theme:scss',
-    'theme:js', 'theme:js:lib', 'theme:js:worker', 'theme:js:gettext']);
+const themeBuild = gulp.series(themeFiles, themeSVG, themeScripts, themeScss, themeJS, themeJSLib, themeJSWorker, themeJSgetText);
 
-gulp.task('theme:watch', ['theme:build'], () => {
-    gulp.watch('src/theme/{*.py,{static,templates}/**}', ['theme:files']);
-    gulp.watch('src/theme/svg/**', ['theme:svg']);
-    gulp.watch('src/theme/scripts/**', ['theme:scripts']);
-    gulp.watch('src/theme/scss/**', ['theme:scss']);
-    gulp.watch('src/theme/js/*.js', ['theme:js', 'theme:js:gettext']);
-    gulp.watch('src/theme/js/offline/worker.js', ['theme:js:worker']);
-});
-
-gulp.task('theme', ['theme:watch']);
+exports.theme = themeBuild;
+exports.clean = clean;
+exports['theme:build'] = themeBuild;
+exports['theme:watch'] = watch;
+exports.theme = watch;
 
 // Homepage
 let renderData = null;
 
-gulp.task('homepage:load-data', () =>
-    !renderData && require('./src/homepage/data/spongedocs').loadData()
-        .then(data => renderData = data)
-);
 
-gulp.task('homepage:html', ['homepage:load-data'], () =>
-    gulp.src('src/homepage/html/*.html')
+function homepageLoadData(cb) {
+    !renderData && require('./src/homepage/data/spongedocs').loadData()
+        .then(data => {
+            renderData = data
+            cb();
+        });
+}
+
+const homepageHTML = gulp.series(homepageLoadData, function homepageHtml() {
+    return gulp.src('src/homepage/html/*.html')
         .pipe(nunjucks.compile(renderData))
         .pipe(htmlmin({
             collapseBooleanAttributes: true,
@@ -130,49 +139,53 @@ gulp.task('homepage:html', ['homepage:load-data'], () =>
             sortClassName: true,
             useShortDoctype: true
         }))
-        .pipe(gulp.dest('dist/homepage'))
-);
+        .pipe(gulp.dest('dist/homepage'));
+});
 
-gulp.task('homepage:scss', () =>
-    gulp.src('src/homepage/scss/spongedocs.scss')
+function homepageScss() {
+    return gulp.src('src/homepage/scss/spongedocs.scss')
         .pipe(sass())
         .pipe(postcss([autoprefixer()]))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/homepage/_static/css'))
-);
+        .pipe(gulp.dest('dist/homepage/_static/css'));
+}
 
-gulp.task('homepage:build', ['homepage:html', 'homepage:scss']);
+function homepageWatch() {
+    gulp.watch('src/homepage/html/**', homepageHTML);
+    gulp.watch('src/homepage/scss/**', homepageScss);
+}
 
-gulp.task('homepage:watch', ['homepage:build'], () => {
-    gulp.watch('src/homepage/html/**', ['homepage:html']);
-    gulp.watch('src/homepage/scss/**', ['homepage:scss']);
+const homepageBuild = gulp.series(homepageHTML, homepageScss);
+
+exports['homepage:build'] = homepageBuild;
+exports['homepage:watch'] = homepageWatch;
+
+const homepageWebserver = gulp.parallel(homepageWatch, function homepageWebserver() {
+    connect.server({
+        root: 'dist/homepage',
+        livereload: true
+    });
 });
 
-gulp.task('homepage:webserver', ['homepage:watch'], () => {
-    gulp.src('dist/homepage')
-        .pipe(webserver({
-            open: true,
-            livereload: true
-        }))
-});
+exports['homepage:webserver'] = homepageWebserver;
+exports.homepage = homepageWebserver;
+exports.build = gulp.series(themeBuild, homepageBuild);
+exports.default = this.build;
 
-gulp.task('homepage', ['homepage:webserver']);
-
-gulp.task('build', ['theme:build', 'homepage:build']);
-gulp.task('default', ['build']);
 
 function shell(plugin, command, args) {
-    return (done) =>
+    return new Promise((resolve, reject) => {
         spawn(command, args, {stdio: 'inherit'})
             .on('error', (err) => {
-                done(new gutil.PluginError(plugin, err))
+                reject(new PluginError(plugin, err))
             })
             .on('exit', (code) => {
-                if (code == 0) {
+                if (code === 0) {
                     // Process completed successfully
-                    done()
+                    resolve()
                 } else {
-                    done(new gutil.PluginError(plugin, `Process failed with exit code ${code}`));
+                    reject(new PluginError(plugin, `Process failed with exit code ${code}`));
                 }
             })
+    });
 }
