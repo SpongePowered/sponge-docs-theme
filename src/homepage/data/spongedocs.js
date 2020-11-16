1e89e830d029f72d215019c5ca5a207ae53f4b2c
Felix Kleinekathöfer <git@felixoi.com>
2020-11-16 09:34:50 +0100

    Publish the hompage as a docker image and updated more scripts (#36)
    * Docker
    
    * Actions
    
    * Fixed docker tag
    
    * TestPyPi
    
    * Use cache in docker publishing
    
    * Made list-translations branch specific
    
    * Fixed list-translations
    
    * Bumped version
    
    * Fix exiting on invalid branch
    
    * Bumped version
    
    * Fixed some languages
    
    * Fix for cldr-localenames-modern
    
    * Try to fix this hell
    
    * Debug
    
    * This is driving me crazy
    
    * Another fix
    
    * Adding it would be better
    
    * Removed legacy versions
    
    * Suppress warnings
    
    * Changed name of flag and disables caching of docker layers
    
    * Remove debug and changed repo
    
    * Set correct tags to docker image

--- src/homepage/data/spongedocs.js
+++ src/homepage/data/spongedocs.js
@@ -1,36 +1,45 @@
 const
     cldr = require('./cldr'),
-    github = require('./github'),
     crowdin = require('./crowdin');
 
-function prepareData(cldr, versions, languages, localeMappings) {
-    console.log(`Loaded ${versions.length} versions and ${languages.length} supported languages`);
+const baseUrl = process.env.BASE_URL;
+
+function prepareData(cldr, languages, localeMappings) {
+    let versions = Object.keys(languages);
+    versions.sort().reverse()
 
     // Add native language names to each language
-    for (language of languages) {
-        const localeCode = localeMappings[language.code];
-        language.localeCode = localeCode.replace('-', '_');
+    for (version of versions) {
+        const versionLanguages = languages[version];
+        console.log(`Loaded version ${version} with ${versionLanguages.length + 1} supported languages`);
 
-        const name = cldr.getDisplayName(language.code, localeCode);
-        language.displayName = name || language.name;
+        for(language of versionLanguages) {
+            const locale = localeMappings[language.id].locale;
+            language.locale = locale.replace('-', '_');
+
+            language.name = localeMappings[language.id].name;
+
+            const name = cldr.getDisplayName(language.locale, locale);
+            language.displayName = name || language.name;
+        }
+
+        // Add source language
+        languages[version].unshift({
+            name: "English",
+            displayName: "English",
+            id: 'en',
+            locale: 'en'
+        });
     }
-
-    // Add source language
-    languages.unshift({
-        name: "English",
-        displayName: "English",
-        code: 'en',
-        localeCode: 'en'
-    });
 
     const mainVersion = versions.shift();
 
     return {
-        mainVersion,
+        baseUrl, mainVersion,
         versions, languages
     };
 }
 
 module.exports.loadData = (done) =>
-    Promise.all([cldr.load(), github.getVersions(), crowdin.getLanguages(), crowdin.getLocaleMappings()])
+    Promise.all([cldr.load(), crowdin.getLanguages(), crowdin.getLocaleMappings()])
         .then(values => prepareData(...values));
